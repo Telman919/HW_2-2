cff1aa105c196378a138b2b7186d854e2e9331a0
Telman919 <F_telman@mail.ru>
2022-09-13 18:28:09 +1000

    wh-2

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const Array = [ 22 , 33 , 44 , 55 , 66 ];
+console.log(Array[2]);
+
+const phone = {
+    color: '#fff',
+    model: 'iphone',
+    price: 50000,
+};
+
+console.log(phone.color);
+console.log(phone.model);
+console.log(phone.price);
